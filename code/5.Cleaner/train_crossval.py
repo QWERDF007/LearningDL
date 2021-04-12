c995fc00b3624d7bb1ca64abc37801c2c03ee6ac
wangtao <595517713@qq.com>
2021-04-12 21:53:20 +0800

    feat(cleanlab): 替换transform的RandomResizedCrop为Resize

--- code/5.Cleaner/train_crossval.py
+++ code/5.Cleaner/train_crossval.py
@@ -375,7 +375,7 @@ def main_worker(gpu, ngpus_per_node, args):
         train_dataset = datasets.ImageFolder(
             traindir,
             transforms.Compose([
-                transforms.RandomResizedCrop(224),
+                transforms.Resize((224, 224)),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 normalize,
@@ -395,7 +395,7 @@ def main_worker(gpu, ngpus_per_node, args):
             val_dataset = datasets.ImageFolder(
                 valdir,
                 transforms.Compose([
-                    transforms.RandomResizedCrop(224),
+                    transforms.Resize((224, 224)),
                     transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),
                     normalize,
